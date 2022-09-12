b63fc3e36308c74c04159008fbc87f4380cc09b2
Maobal <maobal@gmail.com>
2022-09-12 16:38:50 -0500

    Creación del IUsuarioController y el UsuarioController

---
+++ src/main/java/controller/IUsuarioController.java
@@ -0,0 +1,8 @@
+
+package controller;
+import java.util.Map;
+
+    public interface IUsuarioController {
+    public String login(String id_usuario, String contrasena);  
+    
+}
---
+++ src/main/java/controller/UsuarioController.java
@@ -0,0 +1,48 @@
+
+package controller;
+import java.sql.ResultSet;
+import java.sql.Statement;  
+import com.google.gson.Gson;
+import beans.Usuario;
+import connection.DBConnection;
+
+
+public class UsuarioController  implements IUsuarioController {
+     @Override
+    public String login(String id_usuario, String contrasena) {
+
+        Gson gson = new Gson();
+
+        DBConnection con = new DBConnection();
+
+        String sql = "Select * from usuario where id_usuario = '" + id_usuario
+                + "' and contrasena = '" + contrasena + "'";
+
+
+         try {
+            Statement st = con.getConnection().createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                String apellidos = rs.getString("apellidos");
+                String email = rs.getString("email");
+                String direccion = rs.getString("direccion");
+                String ciudad = rs.getString("ciudad");
+                String telefono = rs.getString("telefono");
+                
+
+               Usuario usuario
+                     = new Usuario(id_usuario, contrasena, nombre, apellidos, email, direccion, ciudad,telefono);
+              return gson.toJson(usuario);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            con.desconectar();
+        }
+
+        return "false";
+    }
+    
+}
